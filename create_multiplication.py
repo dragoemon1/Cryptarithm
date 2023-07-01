f2dc17f28366aaf1957d4456cf4e49531d65e269
dragoemon1 <dragoemon32@gmail.com>
2023-07-01 15:00:12 +0900

    first commit

---
+++ create_multiplication.py
@@ -0,0 +1,15 @@
+from cryptarithm import make_problem
+
+STRING = '''
+   ###
+   #4#
+-------
+  ####
+ SAKU
+MONN
+-------
+#2####
+'''
+
+make_problem(STRING, type="m")
+
---
+++ cryptarithm/__init__.py
@@ -0,0 +1,1 @@
+from cryptarithm.cryptarithm import *
